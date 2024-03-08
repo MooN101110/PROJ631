718fa1b5120d3f1c1492f6fbbc2f3553f12ca6e1
Louna Camas <lounacamas@gmail.com>
2024-03-08 15:03:18 +0100

    Recupération et organisation des données
    Question 1 du sujet

--- Projet1_ArbreDecisionnel/quilan.py
+++ Projet1_ArbreDecisionnel/quilan.py
@@ -1,12 +1,18 @@
 import csv
 
-attributs=[]
+attributs={}
+donnees=[]
 with open("golf.csv","r") as f:
     reader = csv.reader(f, delimiter=',')
     for row in reader:
-        if attributs==[]: #revoir condition
+        if attributs=={}: #revoir condition
             for elt in row:
-                attributs.append(elt)
+                attributs.update({elt:[]})
         else:
-            print(', '.join(row))
+            keys=list(attributs.keys())
+            for i,key in enumerate (keys):
+                if row[i] not in attributs[key]:
+                    attributs[key].append(row[i])
+            donnees.append(row)
+
   
